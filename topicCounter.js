7f7075c45ecaf0a93f0854c0f490b15d68197b30
Mariam Mardanghom <mariamm@ntnu.stud.no>
2021-10-31 01:39:50 +0200

    Fixed correct wikipedia api, saved the input as a variable and searched with it using the api.

--- topicCounter.js
+++ topicCounter.js
@@ -1,12 +1,22 @@
-var text = null;
+async function getTopic() {
+    var input = document.getElementById("topicName").value;
 
+    if (input == "" || input == null) {
+        alert("Husk å søke på strenger");
+    } else {
+        //La til &origin=*
+        var textResponse = await fetch("https://en.wikipedia.org/w/api.php?&origin=*&action=parse&section=0&prop=text&format=json&page=" + input, {
+                method: "GET",
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error("Could not reach website.");
+                }
+                return response.json();
+            })
+            .then(json => { var textFile = json['parse']['text']['*']; return textFile })
+            .catch(err => console.error(err));
 
-fetch("https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=dog")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Could not reach website.");
-        }
-        return response.json();
-    })
-    .then(json => console.log(json))
-    .catch(err => console.error(err));
+        console.log(textResponse)
+
+    }
+};
