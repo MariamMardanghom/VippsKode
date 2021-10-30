0fdaf70fb5de246f9e1687e63e43191b5acb8946
Mariam Mardanghom <mariamm@ntnu.stud.no>
2021-10-30 20:28:44 +0200

    get method wikipedias api json file

--- topicCounter.js
+++ topicCounter.js
@@ -1,2 +1,12 @@
+var text = null;
 
-console.log("fjo");
+
+fetch("https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=dog")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Could not reach website.");
+        }
+        return response.json();
+    })
+    .then(json => console.log(json))
+    .catch(err => console.error(err));
